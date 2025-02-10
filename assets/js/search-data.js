ee0864704052c39695968323dffddaf57dbfc58d
Piorosen <aoikazto@naver.com>
2025-02-10 07:35:48 +0000

    Deploying to gh-pages from @ Piorosen/piorosen.github.io@8a3d6af641a5f2b4c6d2e327e213566607da2d07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%61%63%68%61@%75%64%6F%6E.%70%61%72%74%79", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Piorosen", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,7 +520,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=HAp5V-kAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
