c1b9001c7b0deafff0e3a2f44e51330d0ade5f89
Piorosen <aoikazto@naver.com>
2025-02-10 07:59:57 +0000

    Deploying to gh-pages from @ Piorosen/piorosen.github.io@9d260735dcc0ce78b0ab404a6a1db5fe93b7c048 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -178,17 +178,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/implemnt-mnist/";
         
       },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
     },{id: "post-let-39-s-encrypt를-이용하여-시놀로지에-ssl-적용-w-certbot",
       
         title: "Let&#39;s Encrypt를 이용하여 시놀로지에 SSL 적용 (w/ Certbot)",
@@ -693,17 +682,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2022/serial-rs232-cable/";
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
     },{id: "post-pipe를-이용한-다른-프로그램간-통신",
